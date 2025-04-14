756113364e7f6744607d4dd7d8f4d41ed02c3067
Chukwu10 <immanuelchukwu01@gmail.com>
2025-04-15 00:51:39 +0100

    userprofile

---
+++ DAY 1/main.js
@@ -0,0 +1,62 @@
+// A SIMPLE USER PROFILE THAT STORES DIFFERENT DATA TYPES
+
+/*function profile (){
+    let name = 'Chukwu';
+    let age = 25;
+    let isStudent = false;
+    let email;
+    let nickname = null;
+    let accountBalance = 100500050050500240200;
+    let phone = undefined;
+    let isActive = false;
+    let hobbies = [ 'Travelling', 'Cooking', 'Reading', 'Watching movies']
+    let address = {
+        city: 'lagos',
+        Country: "Nigeria"
+
+    }
+
+    return{
+        name, 
+        age,
+        isStudent,
+        email,
+        nickname,
+        accountBalance,
+        phone,
+        isActive,
+        hobbies,
+        address,
+    }
+}
+
+const user = profile ();
+console.log(user)*/
+
+
+function userProfile(){
+    let name = "immanuel Okorie";
+    let age = 25;
+    let isDeveloper = true;
+    let email = undefined;
+    let nickname = null;
+    let skills = ["javascript", "HTML", "CSS"];
+    let location = {city: "lagos", country: "Nigeria"}
+    let accountBalance = 1500000000000000000000n;
+    let uniqueId = Symbol('id');
+
+    return{
+        name,
+        age,
+        isDeveloper,
+        email,
+        nickname,
+        skills,
+        location,
+        accountBalance,
+        uniqueId,
+    }
+}
+
+const profile = userProfile();
+console.log(profile)
